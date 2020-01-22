56967aad6f7f20dcc8dfeeb9e1ae77660c5c5f1f
AntonTarabaev <aa.tarabaev@gmail.com>
2020-01-22 14:06:46 +0300

    Add renderBar function, minor fixes

--- js/stat.js
+++ js/stat.js
@@ -11,6 +11,7 @@ var GRAPH_HEIGHT = 150;
 var COLUMN_GAP = 50;
 var BAR_MAX_HEIGHT = GRAPH_HEIGHT - FONT_GAP;
 var BAR_WIDTH = 40;
+
 var messageTextByLines = ['Ура вы победили!', 'Список результатов:'];
 
 var renderCloud = function (ctx, x, y, width, heigth, color) {
@@ -31,7 +32,7 @@ var renderCloud = function (ctx, x, y, width, heigth, color) {
   ctx.fill();
 };
 
-var canvasTextMultiLines = function (ctx, textsArr, color, font, x, y, lineHeight) {
+var processMultilineCanvas = function (ctx, textsArr, color, font, x, y, lineHeight) {
   for (var i = 0; i < textsArr.length; i++) {
     ctx.fillStyle = color;
     ctx.font = font;
@@ -43,7 +44,7 @@ var canvasTextMultiLines = function (ctx, textsArr, color, font, x, y, lineHeigh
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
-  for (var i = 0; i < arr.length; i++) {
+  for (var i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
@@ -52,27 +53,37 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-var getBarHeight = function (value, maxValue) {
-  return BAR_MAX_HEIGHT * value / maxValue;
+var getBarHeight = function (value, maxValue, barMaxHeight) {
+  return barMaxHeight * value / maxValue;
+};
+
+var getBarColor = function (ctx, name) {
+  if (name === 'Вы') {
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  } else {
+    ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+  }
+};
+
+var renderBar = function (ctx, name, time, x, y, font, fontColor, fontGap, barWidth, barHeight) {
+  ctx.fillStyle = fontColor;
+  ctx.font = font;
+  ctx.textBaseline = 'hanging';
+  ctx.fillText(name, x, y + fontGap / 2);
+  ctx.fillText(Math.ceil(time), x, y - barHeight - fontGap);
+  getBarColor(ctx, name);
+  ctx.fillRect(x, y - barHeight, barWidth, barHeight);
 };
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#ffffff');
 
-  canvasTextMultiLines(ctx, messageTextByLines, '#000000', '16px PT Mono', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP, FONT_GAP);
+  processMultilineCanvas(ctx, messageTextByLines, '#000000', '16px PT Mono', CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP, FONT_GAP);
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = '#000000';
-    ctx.fillText(names[i], CLOUD_X + GRAPH_GAP + (COLUMN_GAP + BAR_WIDTH) * i, CLOUD_HEIGHT + FONT_GAP - GRAPH_GAP);
-    ctx.fillText(Math.ceil(times[i]), CLOUD_X + GRAPH_GAP + (COLUMN_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - (getBarHeight(times[i], maxTime) + GRAPH_GAP + FONT_GAP));
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
-    }
-    ctx.fillRect(CLOUD_X + GRAPH_GAP + (COLUMN_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - (getBarHeight(times[i], maxTime) + GRAPH_GAP), BAR_WIDTH, getBarHeight(times[i], maxTime));
+    renderBar(ctx, names[i], times[i], CLOUD_X + GRAPH_GAP + (COLUMN_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GRAPH_GAP, '16px PT Mono', '#000000', FONT_GAP, BAR_WIDTH, getBarHeight(times[i], maxTime, BAR_MAX_HEIGHT));
   }
 };
