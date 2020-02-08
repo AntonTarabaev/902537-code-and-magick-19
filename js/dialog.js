45d89ffa95c551937bc7b5a7bcd01e6cf052509b
Anton Tarabaev <aa.tarabaev@gmail.com>
2020-02-09 00:50:05 +0300

    loading data from server

--- js/dialog.js
+++ js/dialog.js
@@ -4,6 +4,7 @@
   var userDialog = document.querySelector('.setup');
   var setupClose = userDialog.querySelector('.setup-close');
   var setupOpen = document.querySelector('.setup-open');
+  var form = userDialog.querySelector('.setup-wizard-form');
   var userNameInput = userDialog.querySelector('.setup-user-name');
   var startPosition = {
     x: userDialog.style.left,
@@ -47,11 +48,28 @@
   });
 
   userNameInput.addEventListener('keydown', function (evt) {
-    // Можешь прокомментировать такое решение? Изначально пробовал:
-    // userNameInput.addEventListener('keydown', function (evt) {
-    //  window.util.isEscEvent(evt, evt.stopPropagation);
-    // };
-    // Но это не работало и решил сделать так
     window.util.stopEscPropagation(evt);
+  });
+
+  var onSuccess = function () {
+    userDialog.classList.add('hidden');
+  };
+
+  var onError = function (errorMessage) {
+    var node = document.createElement('div');
+
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), onSuccess, onError);
+    evt.preventDefault();
   });
 })();
